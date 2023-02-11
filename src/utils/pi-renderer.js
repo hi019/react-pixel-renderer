e945361226d6e09b99931d77dec53a48d7987c7c
hi019 <65871571+hi019@users.noreply.github.com>
2023-02-11 14:56:01 -0500

    add pi renderer

---
+++ src/utils/pi-renderer.js
@@ -0,0 +1,38 @@
+import { LedMatrix } from "rpi-led-matrix";
+
+let matrix;
+
+export const init = () => {
+    matrix = new LedMatrix(
+        {
+            ...LedMatrix.defaultMatrixOptions(),
+            rows: 32,
+            cols: 64,
+        },
+        {
+            ...LedMatrix.defaultRuntimeOptions(),
+        }
+    );
+};
+
+export const clear = () => {
+    matrix.clear();
+};
+
+export const drawText = (text, row, col) => {
+    console.log("drawText", text, row, col);
+
+    const x = col * 24;
+    const y = (row + 1) * 24;
+
+    matrix
+        .clear()
+        .brightness(100)
+        .fgColor(0xffffff)
+        .drawText(text, x, y)
+        .sync();
+};
+
+export const drawPixel = (x, y) => {
+    matrix.fgColor(0xffffff).setPixel(x, y).sync();
+};
