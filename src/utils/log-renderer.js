5f76cb707972c4701e3360f2402782236297f754
hi019 <65871571+hi019@users.noreply.github.com>
2023-02-11 15:14:58 -0500

    updates

---
+++ src/utils/log-renderer.js
@@ -0,0 +1,15 @@
+export const init = () => {
+    console.log("init");
+};
+
+export const clear = () => {
+    console.log("clear");
+};
+
+export const drawText = (text, row, col) => {
+    console.log("drawText", text, row, col);
+};
+
+export const drawPixel = (x, y) => {
+    console.log("drawPixel", x, y);
+};
